c97b2df79cfcd4724a1273d02b0d68e1453dead9
rmansurbek2197 <rmansurbek2197@gmail.com>
2026-02-09 20:42:24 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+# 1
+def salom_dunyo():
+    print("Salom, dunyo!")
+
+# 2
+def raqam_korsat():
+    print(100)
+
+# 3
+def motivatsiya():
+    print("Siz muvaffaqiyat sari yurasiz!")
+
+# 4
+def takror_python():
+    print("Python" * 3)
+
+# 5
+def oddiy_hisob():
+    print(5 + 7)
+
+# 6
+def vaqt_xabari():
+    print("Hozir tong!")
+
+# 7
+def fikr():
+    print("O‘rganish - kelajak!")
+
+# 8
+def boshlanish():
+    print("Dastur ishga tushdi!")
+
+# 9
+def tasodifiy_salom():
+    print("Bugun yaxshi kun!")
+
+# 10
+def xush_kelibsiz():
+    print("Xush kelibsiz!")
